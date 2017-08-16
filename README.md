d75bcd42ca5d677e42c5ad0b0535664ceb9a817c
Mohammad Usman Khan <mohammeduk@spartaglobal.co>
2017-08-16 11:11:00 +0100

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# python-server
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: python-server
+# Recipe:: default
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
